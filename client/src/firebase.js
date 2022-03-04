2882447fb255c428372e9759ce3b8933d9b6ce96
Sanjheev R <54068975+sanjheev100@users.noreply.github.com>
2022-03-04 14:06:09 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,12 +4,7 @@ import 'firebase/compat/auth'
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBqn251mp2s4vOPH0J4HFTILdvmZa56eho',
-  authDomain: 'mmg-gym.firebaseapp.com',
-  projectId: 'mmg-gym',
-  storageBucket: 'mmg-gym.appspot.com',
-  messagingSenderId: '844284307831',
-  appId: '1:844284307831:web:c76da845f11c24f3a45972',
+  Your firebase config
 }
 
 // Initialize Firebase
